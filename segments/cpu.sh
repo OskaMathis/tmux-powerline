5e143509c527e1d31f9e1542d91035e7a1644639
OskaMathis <oskamathis@gmail.com>
2020-03-07 17:23:53 +0900

    custom segments

--- segments/cpu.sh
+++ segments/cpu.sh
@@ -6,7 +6,7 @@ run_segment() {
 		cpu_user=$(echo "$cpu_line" | grep -Po "(\d+(.\d+)?)(?=%?\s?(us(er)?))")
 		cpu_system=$(echo "$cpu_line" | grep -Po "(\d+(.\d+)?)(?=%?\s?(sys?))")
 		cpu_idle=$(echo "$cpu_line" | grep -Po "(\d+(.\d+)?)(?=%?\s?(id(le)?))")
-	elif shell_is_osx; then 
+	elif shell_is_osx; then
 		cpus_line=$(top -e -l 1 | grep "CPU usage:" | sed 's/CPU usage: //')
 		cpu_user=$(echo "$cpus_line" | awk '{print $1}'  | sed 's/%//' )
 		cpu_system=$(echo "$cpus_line" | awk '{print $3}'| sed 's/%//' )
@@ -14,7 +14,7 @@ run_segment() {
 	fi
 
 	if [ -n "$cpu_user" ] && [ -n "$cpu_system" ] && [ -n "$cpu_idle" ]; then
-		echo "${cpu_user}, ${cpu_system}, ${cpu_idle}" | awk -F', ' '{printf("%5.1f,%5.1f,%5.1f",$1,$2,$3)}'
+		echo "${cpu_user}, ${cpu_system}, ${cpu_idle}" | awk -F', ' '{printf("%3.0f%%,%3.0f%%,%3.0f%%",$1,$2,$3)}'
 		return 0
 	else
 		return 1
---
+++ segments/datetime.sh
@@ -0,0 +1,4 @@
+run_segment() {
+	date +"%Y/%m/%d(%a) %H:%M"
+	return 0
+}
